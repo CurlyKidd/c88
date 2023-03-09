0da5ad4ca214505b4cd06e16f65963704c801f2e
CurlyKidd <96920739+CurlyKidd@users.noreply.github.com>
2023-03-08 19:39:28 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDM0c8Vn5uovDy5LmIXf_AYnaOzX0QWa2E",
+  authDomain: "storytelling-app-f9fab.firebaseapp.com",
+  databaseURL: "https://storytelling-app-f9fab-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-f9fab",
+  storageBucket: "storytelling-app-f9fab.appspot.com",
+  messagingSenderId: "743346249671",
+  appId: "1:743346249671:web:6ae610662f6d758be403e2"
+  };
